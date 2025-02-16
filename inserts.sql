b0f6566b344a196909503661cd1c795a0d243bae
Andrew Jones <jon20039@byui.edu>
2025-02-15 22:00:13 -0700

    category and address table inserts

--- inserts.sql
+++ inserts.sql
@@ -7,10 +7,32 @@
 ------------------------
 ---- category_table ----
 ------------------------
+START TRANSACTION;
 
+INSERT INTO category_table 
+(name,description)
+VALUES ('Galactic footwear','A collection of space-inspired shoes designed for those who want to walk among the stars. Featuring futuristic styles and cosmic comfort, these shoes are perfect for dreamers and explorers alike.'),
+  ('Astro kicks collection','Elevate your step with the Astro Kicks Collection, where innovation meets interstellar fashion. Designed for both comfort and style, these kicks are ready for any cosmic adventure'),
+  ('Cosmic Stride Series','Step boldly into the future with the Cosmic Stride Series, a lineup of gravity-defying footwear inspired by the mysteries of the universe. Walk with confidence, whether on Earth or beyond.'),
+  ('Interstellar Soles','Experience the next evolution of footwear with Interstellar Soles—a range of celestial designs crafted for comfort, durability, and a style that’s light-years ahead'),
+  ('Nebula Walkers',' Inspired by the beauty of the cosmos, Nebula Walkers bring a touch of the stars to your everyday journey. Sleek, stylish, and infused with futuristic flair, these shoes will take you places—literally and figuratively');
+
+COMMIT;
 -----------------
 ---- address ----
 -----------------
+START TRANSACTION;
+
+INSERT INTO address 
+(street_address, city, state, postal_code, country)
+VALUES 
+    ('123 Pinecrest Dr', 'Boise', 'ID', 83702, 'USA'),
+    ('4567 Maple Ave', 'Idaho Falls', 'ID', 83401, 'USA'),
+    ('890 Riverbend Rd', 'Coeur d'Alene', 'ID', 83804, 'USA'),
+    ('321 Aspen Way', 'Twin Falls', 'ID', 83301, 'USA'),
+    ('6789 Birch Ln', 'Meridian', 'ID', 83642, 'USA');
+
+COMMIT;
 
 -------------------
 ---- discounts ----
