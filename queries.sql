96947a76f4c34c6c639f921af71ed78e6f72bf96
Daniel Anaya <122187600+Daniel-J-Anaya@users.noreply.github.com>
2025-02-24 14:03:38 -0700

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,11 @@
+-- -----------------------------------------
+-- This file is for saving any queries we are using in our DB: Please follow this format.
+
+-- ---------------------
+-- Situation (e.g. Looking for an order that was shipped by purchasers first name.)
+-- ---------------------
+-- Query Statment
+
+-- -----------------------------------------
+
+
