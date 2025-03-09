85ba578ffad777951fced95a9f08ef4d30891c52
Millian19 <millian19@byui.edu>
2025-03-08 22:37:31 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -31,4 +31,24 @@ ORDER BY o.total_price DESC;
 -- This query tells us which orders have been shipped but not delivered, and the amount of days since the order was placed. 
 -- It has the users name and phone number to contact if about the delay in the order.
 
+
 -- --------------------------
+-- Ian Miller Week 09 Query
+-- --------------------------
+-- Want to look at how active customers are giving the total number of orders there are depending on each customer
+
+SELECT 
+    o.user_id,
+    u.first_name, u.last_name,
+    COUNT(o.orders_id) AS 'Total Orders',
+    CASE 
+        WHEN COUNT(o.orders_id) >= 5 THEN 'High Activity'
+        WHEN COUNT(o.orders_id) BETWEEN 2 AND 4 THEN 'Medium Activity'
+        ELSE 'Very Low Activity'
+    END AS 'Activity'
+FROM orders o
+INNER JOIN users u
+    ON o.user_id = u.user_id
+GROUP BY o.user_id, u.first_name, u.last_name;
+
+-- This query returns each user and their respective activity in relation to orders
