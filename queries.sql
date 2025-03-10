4072bb8ad877233b199cbc21998a9c07bcd35008
jacobaoliver <oli24007@byui.edu>
2025-03-10 14:26:02 -0600

    jacob push

--- queries.sql
+++ queries.sql
@@ -7,4 +7,14 @@
 -- Query Statment
 
 -- -----------------------------------------
-
+-- Jacob Oliver Query --
+-- list users and their wishlist items --
+-- -----------------------------------------
+SELECT 
+    CONCAT(u.first_name, ' ', u.last_name) AS user_name,
+    p.product_name
+FROM wishlist w
+JOIN users u ON w.user_id = u.user_id
+JOIN product_table_wishlist pw ON w.wishlist_id = pw.wishlist_id
+JOIN product_table p ON pw.product_table_id = p.product_table_id;
+-- -----------------------------------------
