ed8a5930c1dc471623c76ad7caadd7682272fbcc
Daniel Anaya <122187600+Daniel-J-Anaya@users.noreply.github.com>
2025-03-11 11:58:10 -0600

    Update inserts2.sql

--- inserts2.sql
+++ inserts2.sql
@@ -1,3 +1,21 @@
+-- FORMAT
+
+-- Team Member: Description
+-- START TRANSACTION;
+
+-- INSERT INTO table name (table columns)
+-- VALUES
+--     ('Value 1', 'Value 2'),
+--     ('Value 1', 'Value 2')
+
+-- COMMIT;
+
+-- ---------------------------------------------------------------
+
+-- -----------------------------------
+-- Efrain Miller: Additional Addresses
+-- -----------------------------------
+START TRANSACTION;
 
 INSERT INTO address (street_address, city, state, postal_code, country)
 VALUES 
@@ -6,7 +24,15 @@ VALUES
     ('789 Pine Road', 'Los Angeles', 'California', '90001', 'United States'),
     ('101 Birch Lane', 'Miami', 'Florida', '33101', 'United States'),
     ('202 Cedar Boulevard', 'New York', 'New York', '10001', 'United States');
-SELECT * FROM address;
+
+COMMIT;
+
+-- ---------------------------------------------------------------
+
+-- -----------------------------------
+-- Efrain Miller: Additional Users
+-- -----------------------------------
+START TRANSACTION;
 
  INSERT INTO users (first_name,last_name,email,password_hash,phone_number,address_id,created_at)
  VALUES 
@@ -15,3 +41,5 @@ SELECT * FROM address;
 ('Michael', 'Johnson', 'michaeljohnson@gmail.com', 'F2g3H4i5J6', '+2109876543', '8', '2025-03-10'),
 ('Emily', 'Davis', 'emilydavis@gmail.com', 'K7l8M9n0O1', '+2112345678', '9', '2025-03-10'),
 ('David', 'Wilson', 'davidwilson@gmail.com', 'P3q4R5s6T7', '+2123456789', '10', '2025-03-10');
+
+COMMIT;
